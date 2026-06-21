0cd3784b1b63b6ad8331928d238eda07b568b156
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2026-06-21 06:54:07 +0500

    Update DbContext comment in Program.cs

--- TankerManagementSystem/Program.cs
+++ TankerManagementSystem/Program.cs
@@ -4,7 +4,7 @@ using TankerManagementSystem.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 
-// DbContext old
+// DbContext New
 builder.Services.AddDbContext<TankerDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
 
